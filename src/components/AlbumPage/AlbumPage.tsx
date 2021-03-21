a90491104db6a09bca90dfe98ee3181c7b8b59ad
haris0 <hari.surrisyad@gmail.com>
2021-03-21 16:21:24 +0700

    improve code spliting

--- src/components/AlbumPage/AlbumPage.tsx
+++ src/components/AlbumPage/AlbumPage.tsx
@@ -21,28 +21,24 @@ const AlbumPage = () => {
   return (
     <div className='album-page'>
       <Header/>
-      <div className="cover">
-        <Container>
-          {loading &&
-            <>
-              <div>
-                <Skeleton widthSize='350px' heigthSize='40px'/>
-              </div>
-              <div style={{marginTop:'15px'}}>
-                <Skeleton widthSize='120px' heigthSize='20px'/>
-              </div>
-            </>
-          }
-          {!loading && data &&
-            <AlbumCover album={data.data} user={data.data.userData} />
-          }
-        </Container>
-      </div>
+      {loading &&
+        <div className="cover">
+          <Container>
+            <div>
+              <Skeleton widthSize='350px' heigthSize='40px'/>
+            </div>
+            <div style={{marginTop:'15px'}}>
+              <Skeleton widthSize='120px' heigthSize='20px'/>
+            </div>
+          </Container>
+        </div>
+      }
       {!loading && data &&
         <>
-        <Container className='margin' style={{marginTop:'20px'}}>
-          <PhotoCard photoList={data.data.photos}/>
-        </Container>
+          <AlbumCover album={data.data} user={data.data.userData} />
+          <Container className='margin' style={{marginTop:'20px'}}>
+            <PhotoCard photoList={data.data.photos}/>
+          </Container>
         </>
       }
       {!loading && error &&
