98fca0d4c5cfc340c3bc6b55eb613133431def92
haris0 <hari.surrisyad@gmail.com>
2021-03-22 09:57:33 +0700

    handle error and loading proses on detail page

--- src/components/DetailPhoto/DetailPhoto.tsx
+++ src/components/DetailPhoto/DetailPhoto.tsx
@@ -11,6 +11,8 @@ import { Container,
          Row,
          Col} from 'react-bootstrap'
 import send from '../../assets/send.png'
+import ErrorText from '../Reuseable/ErrorText'
+import Skeleton from '../Reuseable/Skeleton'
 
 const DetailPhoto = () => {
   interface ParamTypes {
@@ -42,14 +44,45 @@ const DetailPhoto = () => {
   return (
     <div className='detail-photo'>
       <Header/>
+      {loading &&
+        <Container className='container-margin'>
+          <Row>
+            <Col sm={4}>
+              <div className='photo-div'>
+                <Skeleton widthSize='100%' paddingTop='100%'/>
+                <div className="photo-title">
+                  <Skeleton widthSize='100%' heigthSize='40px'/>
+                </div>
+              </div>
+            </Col>
+            <Col sm={8}>
+              <div className="comment-div">
+                <div className="comment-title">
+                  Photo's Comment
+                </div>
+                  <div className="comment">
+                    <div style={{marginBottom:'10px'}}>
+                      <Skeleton widthSize='100px' heigthSize='20px'/>
+                    </div>
+                    <div className="commnet-text">
+                      <Skeleton widthSize='50%' heigthSize='20px'/>
+                    </div>
+                    <hr/>
+                  </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      }
       {!loading && data &&
+        <>
         <Container className='container-margin'>
           <Row>
             <Col sm={4}>
               <div className='photo-div'>
                 <img
                   src={data.data.url} 
-                  alt="Photo"
+                  alt="Detail"
                   className='photo-detail'/>
                 <div className="photo-title">{data.data.title}</div>
               </div>
@@ -70,27 +103,31 @@ const DetailPhoto = () => {
             </Col>
           </Row>
         </Container>
+        <div className="comment-input">
+          <Container>
+            <Form onSubmit={(e)=> handleSupmit(e)}>
+              <InputGroup className="mb-3 margin-input">
+                <FormControl
+                  placeholder="Comment"
+                  aria-label="Comment"
+                />
+                <InputGroup.Append>
+                  <Button 
+                    variant="outline-secondary" 
+                    type='submit'
+                    className='send-button'>
+                    <img src={send} alt="send" className='send-img'/>
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </Form>
+          </Container>
+        </div>
+        </>
       }
-      <div className="comment-input">
-        <Container>
-          <Form onSubmit={(e)=> handleSupmit(e)}>
-            <InputGroup className="mb-3 margin-input">
-              <FormControl
-                placeholder="Comment"
-                aria-label="Comment"
-              />
-              <InputGroup.Append>
-                <Button 
-                  variant="outline-secondary" 
-                  type='submit'
-                  className='send-button'>
-                  <img src={send} alt="send" className='send-img'/>
-                </Button>
-              </InputGroup.Append>
-            </InputGroup>
-          </Form>
-        </Container>
-      </div>
+      {!loading && error &&
+        <ErrorText/>
+      }
     </div>
   )
 }
--- src/components/Reuseable/Skeleton.tsx
+++ src/components/Reuseable/Skeleton.tsx
@@ -3,12 +3,13 @@ import './Skeleton.scss'
 
 type propType = {
   widthSize:string,
-  heigthSize:string
+  heigthSize?:string,
+  paddingTop?:string
 }
 
-const Skeleton = ({widthSize, heigthSize}:propType) => {
+const Skeleton = ({widthSize, heigthSize, paddingTop}:propType) => {
   return (
-    <div className='skeleton' style={{width:widthSize, height:heigthSize}}/>
+    <div className='skeleton' style={{width:widthSize, height:heigthSize, paddingTop:paddingTop}}/>
   )
 }
 
