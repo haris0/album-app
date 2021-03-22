d13bcb667b6bfe5b29a78378b7791b6ef3be574e
haris0 <hari.surrisyad@gmail.com>
2021-03-22 10:03:59 +0700

    code splitting detail photo page

--- src/components/DetailPhoto/DetailPhoto.tsx
+++ src/components/DetailPhoto/DetailPhoto.tsx
@@ -12,7 +12,7 @@ import { Container,
          Col} from 'react-bootstrap'
 import send from '../../assets/send.png'
 import ErrorText from '../Reuseable/ErrorText'
-import Skeleton from '../Reuseable/Skeleton'
+import DetailPhotoSkeleton from './child/DetailPhotoSkeleton'
 
 const DetailPhoto = () => {
   interface ParamTypes {
@@ -45,34 +45,7 @@ const DetailPhoto = () => {
     <div className='detail-photo'>
       <Header/>
       {loading &&
-        <Container className='container-margin'>
-          <Row>
-            <Col sm={4}>
-              <div className='photo-div'>
-                <Skeleton widthSize='100%' paddingTop='100%'/>
-                <div className="photo-title">
-                  <Skeleton widthSize='100%' heigthSize='40px'/>
-                </div>
-              </div>
-            </Col>
-            <Col sm={8}>
-              <div className="comment-div">
-                <div className="comment-title">
-                  Photo's Comment
-                </div>
-                  <div className="comment">
-                    <div style={{marginBottom:'10px'}}>
-                      <Skeleton widthSize='100px' heigthSize='20px'/>
-                    </div>
-                    <div className="commnet-text">
-                      <Skeleton widthSize='50%' heigthSize='20px'/>
-                    </div>
-                    <hr/>
-                  </div>
-              </div>
-            </Col>
-          </Row>
-        </Container>
+        <DetailPhotoSkeleton/>
       }
       {!loading && data &&
         <>
